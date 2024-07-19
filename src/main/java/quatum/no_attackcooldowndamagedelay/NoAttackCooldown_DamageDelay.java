46bd4b0faaba4a39204db98ebf969e5b4dc49341
Quantum-mutnauQ <141065355+Quantum-mutnauQ@users.noreply.github.com>
2024-07-19 11:14:02 +0200

    Moved to NeoForge

---
+++ src/main/java/quatum/no_attackcooldowndamagedelay/NoAttackCooldown_DamageDelay.java
@@ -0,0 +1,23 @@
+package quatum.no_attackcooldowndamagedelay;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NoAttackCooldown_DamageDelay.MODID)
+public class NoAttackCooldown_DamageDelay {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "no_attack_cooldown_damage_delay";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public NoAttackCooldown_DamageDelay(IEventBus modEventBus, ModContainer modContainer){
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON,Config.SPEC);
+    }
+}
