7a7d0d95ae084c63fb6bb5dcda7bb76442f1f6a8
Quantum-mutnauQ <unconfigured@null.spigotmc.org>
2025-01-26 17:49:08 +0100

    worked on 1.21.0 - 1.21.4

--- no attack cooldown - damage delay 1.21 - 1.21.4/src/main/java/quatum/no_attackcooldowndamagedelay/NoAttackCooldown_DamageDelay.java
+++ no attack cooldown - damage delay 1.21 - 1.21.4/src/main/java/quatum/no_attackcooldowndamagedelay/NoAttackCooldown_DamageDelay.java
@@ -1,17 +1,10 @@
 package quatum.no_attackcooldowndamagedelay;
 
 import com.mojang.logging.LogUtils;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
