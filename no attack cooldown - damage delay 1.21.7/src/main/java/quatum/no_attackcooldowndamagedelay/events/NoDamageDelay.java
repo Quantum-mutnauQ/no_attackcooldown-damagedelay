89564fce86b9d243e8d5acee96dcd610db457cff
Quantum-mutnauQ <unconfigured@null.spigotmc.org>
2025-07-01 16:21:57 +0200

    added support for 1.21.7

---
+++ no attack cooldown - damage delay 1.21.7/src/main/java/quatum/no_attackcooldowndamagedelay/events/NoDamageDelay.java
@@ -0,0 +1,31 @@
+package quatum.no_attackcooldowndamagedelay.events;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import quatum.no_attackcooldowndamagedelay.Config;
+import quatum.no_attackcooldowndamagedelay.NoAttackCooldown_DamageDelay;
+
+import java.util.Objects;
+
+@EventBusSubscriber
+public class NoDamageDelay {
+    @SubscribeEvent
+    public static void onLivingDamage(LivingDamageEvent.Post event) {
+        if (event == null || event.getEntity() == null) {
+            return;
+        }
+
+        String damageType = Config.damageType_to_CorospoigString(Objects.requireNonNull(event.getSource().typeHolder().unwrapKey().get()));
+
+        if (Config.LogDamageValue) {
+            NoAttackCooldown_DamageDelay.LOGGER.info(damageType);
+        }
+
+        if (Config.NoDamageDelayValue && !event.getEntity().level().isClientSide()) {
+            if (!Config.damageTypesListValue.contains(damageType)) {
+                event.getEntity().invulnerableTime = 0;
+            }
+        }
+    }
+}
