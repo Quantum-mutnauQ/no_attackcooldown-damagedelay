138d88eafe6e4ac6d3e2ba665f7a7069c36afe12
Quantum-mutnauQ <unconfigured@null.spigotmc.org>
2026-07-02 19:29:59 +0200

    added support for 26.2

---
+++ no attack cooldown - damage delay 26.2/src/main/java/quatum/no_attackcooldowndamagedelay/events/NoDamageDelay.java
@@ -0,0 +1,34 @@
+package quatum.no_attackcooldowndamagedelay.events;
+
+import net.minecraft.client.gui.components.DebugScreenOverlay;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
+import quatum.no_attackcooldowndamagedelay.Config;
+import quatum.no_attackcooldowndamagedelay.NoAttackCooldown_DamageDelay;
+
+import java.util.Objects;
+
+@EventBusSubscriber
+public class NoDamageDelay {
+    @SubscribeEvent
+    public static void onLivingDamage(LivingIncomingDamageEvent event) {
+        if (event == null || event.getEntity() == null) {
+            return;
+        }
+
+        String damageType = Config.damageType_to_CorospoigString(Objects.requireNonNull(event.getSource().typeHolder().unwrapKey().get()));
+        var sourceEntity = event.getSource().getEntity();
+
+        if (Config.LogDamageValue) {
+            NoAttackCooldown_DamageDelay.LOGGER.info(damageType);
+        }
+
+        if (Config.NoDamageDelayValue && !event.getEntity().level().isClientSide()) {
+            if (!Config.damageTypesListValue.contains(damageType)&&(sourceEntity == null || !Config.blacklistedEntitysValue.contains(Config.entity_to_CotospoigStrig(sourceEntity)))) {
+                event.getEntity().invulnerableTime = 0;
+            }
+        }
+    }
+}
